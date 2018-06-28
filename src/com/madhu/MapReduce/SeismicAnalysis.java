497e7b6e2d9a58ea937b1a30af7c48f4dc13e7b6
Madhulica Singh <singh.madhulica@gmail.com>
2018-06-28 21:33:08 +0530

    My old working version

--- src/com/madhu/MapReduce/SeismicAnalysis.java
+++ src/com/madhu/MapReduce/SeismicAnalysis.java
@@ -24,79 +24,80 @@ public class SeismicAnalysis {
 
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, FloatWritable> {
 
-		@Override
-		public void map(LongWritable key, Text value, OutputCollector<Text, FloatWritable> output, Reporter reporter)
-				throws IOException {
+@Override
+public void map(LongWritable key, Text value, OutputCollector<Text,FloatWritable> output, Reporter reporter)
+		throws IOException {
 
-			// skip first row which has only headers
-			if (key.get() == 0) {
-				return;
-			}
-
-			String line = value.toString();
-
-			StringTokenizer tokenizer = new StringTokenizer(line, ",");
-
-			// Skip 8 fields
-			for (int i = 0; i < 8; i++) {
-				tokenizer.nextToken();
-			}
-			// use 9th field
-			FloatWritable magnitude = new FloatWritable(Float.valueOf(tokenizer.nextToken()));
+	//skip first row which has only headers
+	if(key.get() == 0) {
+		return;
+	}
 	
-			// Skip 10-11th fields
-			tokenizer.nextToken();
-			tokenizer.nextToken();
+	String line = value.toString();
+	
+	StringTokenizer tokenizer = new StringTokenizer(line,",");
+
 		
-			// use 12th field
-			Text region = new Text(tokenizer.nextToken());
+	//Check the data fields, the 8th and 11th fields are required
+	for(int i =0;i<8;i++) {
+		tokenizer.nextToken();
+	}
+	
+	FloatWritable magnitude = new FloatWritable(Float.valueOf(tokenizer.nextToken()));
+	tokenizer.nextToken();
+	tokenizer.nextToken();
+	Text region = new Text(tokenizer.nextToken());
 
-			output.collect(region, magnitude);
+	output.collect(region,magnitude);
+}
+}
+
+
+public static class Reduce extends MapReduceBase implements
+	Reducer<Text, FloatWritable, Text, FloatWritable> {
+
+@Override
+public void reduce(Text key, Iterator<FloatWritable> values, OutputCollector<Text, FloatWritable> output, Reporter reporter)
+		throws IOException {
+
+	float maxMag = 0;//initialize
+	
+	while(values.hasNext()) {
+		float x = values.next().get();
+		if(maxMag < x) {
+			maxMag =x;
 		}
 	}
 
-	public static class Reduce extends MapReduceBase implements Reducer<Text, FloatWritable, Text, FloatWritable> {
+	output.collect(key, new FloatWritable(maxMag));
+}
+}
 
-		@Override
-		public void reduce(Text key, Iterator<FloatWritable> values, OutputCollector<Text, FloatWritable> output,
-				Reporter reporter) throws IOException {
+public static void main(String[] args) throws Exception {
 
-			float maxMag = 0;// initialize
+JobConf conf = new JobConf(SeismicAnalysis.class);
+conf.setJobName("seismic");
 
-			while (values.hasNext()) {
-				float x = values.next().get();
-				if (maxMag < x) {
-					maxMag = x;
-				}
-			}
+// Forcing program to run 5 reducers
+conf.setNumReduceTasks(1);
 
-			output.collect(key, new FloatWritable(maxMag));
-		}
-	}
+conf.setMapperClass(Map.class);
+conf.setCombinerClass(Reduce.class);
+conf.setReducerClass(Reduce.class);
+//conf.setPartitionerClass(MyPartitioner.class);
 
-	public static void main(String[] args) throws Exception {
+conf.setOutputKeyClass(Text.class);
+conf.setOutputValueClass(FloatWritable.class);
 
-		JobConf conf = new JobConf(SeismicAnalysis.class);
-		conf.setJobName("seismic");
+conf.setInputFormat(TextInputFormat.class);
+conf.setOutputFormat(TextOutputFormat.class);
 
-		// Forcing program to run 5 reducers
-		conf.setNumReduceTasks(1);
-
-		conf.setMapperClass(Map.class);
-		conf.setCombinerClass(Reduce.class);
-		conf.setReducerClass(Reduce.class);
-		// conf.setPartitionerClass(MyPartitioner.class);
-
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(FloatWritable.class);
-
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-		JobClient.runJob(conf);
-	}
-
+ FileInputFormat.setInputPaths(conf, new Path(args[0]));
+ FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+ 
+JobClient.runJob(conf);
+}
+	
+	
+	
 }
